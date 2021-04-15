e678f0b9474f33b9bdd8d605304452c1919040cf
100lvlmaster <navinkodag@gmail.com>
2021-04-16 02:56:19 +0530

    update_example_app

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,16 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_twitter_login/flutter_twitter_login.dart';
 
-void main() => runApp(new MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
   @override
-  _MyAppState createState() => new _MyAppState();
+  _MyAppState createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-  static final TwitterLogin twitterLogin = new TwitterLogin(
+  static final TwitterLogin twitterLogin = TwitterLogin(
     consumerKey: 'kkOvaF1Mowy4JTvCxKTV5O1WF',
     consumerSecret: 'ZECGsI6UUDBEUVGkJe4S5vd0FGqGxC3wMJCgsXgPRfjSwRFnyH',
   );
@@ -19,7 +18,7 @@ class _MyAppState extends State<MyApp> {
 
   void _login() async {
     final TwitterLoginResult result = await twitterLogin.authorize();
-    String newMessage;
+    String newMessage = 'Login error: ${result.errorMessage}';
 
     switch (result.status) {
       case TwitterLoginStatus.loggedIn:
@@ -48,22 +47,22 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text('Twitter login sample'),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Twitter login sample'),
         ),
-        body: new Center(
-          child: new Column(
+        body: Center(
+          child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              new Text(_message),
-              new RaisedButton(
-                child: new Text('Log in'),
+              Text(_message),
+              MaterialButton(
+                child: Text('Log in'),
                 onPressed: _login,
               ),
-              new RaisedButton(
-                child: new Text('Log out'),
+              MaterialButton(
+                child: Text('Log out'),
                 onPressed: _logout,
               ),
             ],
